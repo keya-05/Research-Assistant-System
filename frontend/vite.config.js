8aa4c37ed54f83c24d674ac4a7a1e69fd55bbc62
Sanyaktabhatt <sanyakta27@gmail.com>
2026-05-01 01:58:55 +0530

    fix railway vite host issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 5173,
+    allowedHosts: true,
+
     proxy: {
-      '/query':  'http://backend:8000',
+      '/query': 'http://backend:8000',
       '/health': 'http://backend:8000',
     }
   }
